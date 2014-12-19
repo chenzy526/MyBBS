d1ead2f3f88fc57afee0a72bc2b08e0db4467024
chenzy526 <czyhelcj@163.com>
2014-12-19 11:11:40 +0800

    Revert "20141219"
    This reverts commit a2d044736216139664aff9198b56aa4d8fcd2d47.

--- OAS/OA/Home/Conf/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-	
-// 		'SHOW_PAGE_TRACE' => true,
-	'DEFAULT_MODULE'  =>  'Home',
- 	'DEFAULT_CONTROLLER'    =>  'Login', 
-// 	'DEFAULT_ACTION'        =>  'index',
-    );
-?>
